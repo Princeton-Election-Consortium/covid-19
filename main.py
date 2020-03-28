b823b8c03f5af0d69b411bcbb5b3c16d92305178
ben <bendeverett@gmail.com>
2020-03-27 22:37:26 -0400

    update to auto-create directories for data & images

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ This script will be run once per day(?) using a cron job on the PEC server.
 It is run from using the run.sh bash script which first pulls down new data.
 """
 
+import os
 from scrape import scrape_all_regions
 from calculations import compute_fold_change, compute_top_n
 from displays import generate_foldchange_plot, create_html
@@ -17,6 +18,10 @@ from displays import generate_foldchange_plot, create_html
 var_to_track = 'Deaths'
 scraped_data_filename = f'data/scraped_data-{var_to_track}.csv'
 foldchange_filename = f'data/fold_change-{var_to_track}.csv'
+
+# Setup
+os.makedirs('data', exist_ok=True)
+os.makedirs('images', exist_ok=True)
 
 ## Step 1: scrape and save to file
 data = scrape_all_regions(var_to_track=var_to_track)
