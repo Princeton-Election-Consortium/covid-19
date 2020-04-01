2d7f81430d1fe619ea7a9b62663523d5b0df393a
Lucas Manning <lucas.manning21@gmail.com>
2020-03-31 22:06:45 -0400

    adding data output and auto-pull to run script

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Pull new data
+git pull origin master
 cd COVID-19
 git pull origin master
 cd ..
@@ -8,3 +9,9 @@ cd ..
 # Run pipeline
 python3 main.py deaths
 python3 main.py confirmed
+
+# copy output folders
+cp images/* "/web/www/data/covid-19/today"
+mkdir "/web/www/data/covid-19/$(date +"%d-%m-%Y")"
+cp images/* "/web/www/data/covid-19/$(date +"%d-%m-%Y")"
+cp data/* "/web/www/data/covid-19/"
