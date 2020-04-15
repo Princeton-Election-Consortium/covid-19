08ba9919a08c2f254acc07e56811b0465ca9c505
Lucas Manning <lucas.manning21@gmail.com>
2020-04-14 22:06:50 -0400

    fixed run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # Pull new data
+cd /web/covid-19
 git pull origin master
-cd COVID-19
+cd /web/covid-19/COVID-19
 git pull origin master
-cd ..
+cd /web/covid-19
+
+[ ! -d "/web/www/data/covid-19/$(date +"%d-%m-%Y")" ] && mkdir "/web/www/data/covid-19/$(date +"%d-%m-%Y")"
 
 # Run pipeline
 python3 main.py deaths
@@ -12,6 +15,6 @@ python3 main.py confirmed
 
 # copy output folders
 cp images/* "/web/www/data/covid-19/today"
-mkdir "/web/www/data/covid-19/$(date +"%d-%m-%Y")"
 cp images/* "/web/www/data/covid-19/$(date +"%d-%m-%Y")"
 cp data/* "/web/www/data/covid-19/"
+~
